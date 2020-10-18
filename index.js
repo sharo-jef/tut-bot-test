6947fdf4f4427e1b144f8c15be6a6823fbd30317
sharo-jef <cleared.for.approach.runway23@gmail.com>
2020-10-18 11:08:17 +0900

    updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import express from 'express'
 import line from '@line/bot-sdk'
 
-const BACKEND_SERVER_URI_BASE = 'https://';
+const BACKEND_SERVER_URI_BASE = 'https://bot-php-api.herokuapp.com/api.php';
 const config = {
     channelAccessToken: 'oThSS0dRpWWNegfDp5RO6MPtiNYfeWwmcOJt93e/pwrD1Tx6qGBJapiHCmZjH+GWJ5oCyLWvvaLxeq+RpGlz1wNOU98pNaZ+lrNOec9zSkhbOFw+rSfLQ5RtVShadp/UFCIB7EFTsGf+kUFGLxM2NQdB04t89/1O/w1cDnyilFU=',
     channelSecret: '589de7a74a4bcbab30afe84b55ab193d',
@@ -28,6 +28,6 @@ app.get('/debug', (req, res) => {
 async function main(req, res) {
     res.status(200).end();
     console.log(req.body);
-    // const response = axios.post(BACKEND_SERVER_URI_BASE, req.body);
-    // console.log(response);
+    const response = axios.post(BACKEND_SERVER_URI_BASE, req.body);
+    console.log(response);
 }
