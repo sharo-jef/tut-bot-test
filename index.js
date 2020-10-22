01f0e6af5bff10166fca7b67daec36f0fd9aadec
sharo-jef <cleared.for.approach.runway23@gmail.com>
2020-10-23 02:06:25 +0900

    updated

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.get('/debug', (req, res) => {
  */
 async function main(req, res) {
     res.status(200).end();
-    console.log(JSON.parse(req.body));
+    console.log(typeof req.body);
     const response = await axios.post(BACKEND_SERVER_URI_BASE, req.body);
     console.log(response.data);
 }
